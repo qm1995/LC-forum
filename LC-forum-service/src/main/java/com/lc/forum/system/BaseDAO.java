90113e0407bbf821026ba85d26b1cb0226a7009c
qm1995 <1075812593@qq.com>
2018-10-30 19:44:42 +0800

    fix

---
+++ LC-forum-service/src/main/java/com/lc/forum/system/BaseDAO.java
@@ -0,0 +1,11 @@
+package com.lc.forum.system;
+
+import tk.mybatis.mapper.common.Mapper;
+
+/**
+ * @author qiumin
+ * @create 2018/10/28 21:58
+ * @desc
+ **/
+public interface BaseDAO<T> extends Mapper<T> {
+}
